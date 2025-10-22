49c1293997e055382f82093cb74410fd2911e087
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-10-22 22:23:32 +0100

    adding load students_raw

---
+++ ProgrammingforAI/CA/analysis.py
@@ -0,0 +1,16 @@
+# Code developed by Renato Francisco Goedert
+# Sutudent Number: x25115766
+
+import pandas as pd
+import numpy as np
+from colorama import Fore, Style, init
+
+# Initialising Colorama, I like Colors, dont judge me!
+init(autoreset=True)
+
+# Try to load the dataset
+try:
+    df = pd.read_csv('students_raw.csv')
+    print(f"{"\033[38;5;46m"} Succesfully loaded students_raw.csv")
+except FileNotFoundError:
+    print(f"{Fore.RED} ERROR: file not found. Make sure you have run python \"student_data.py\" ")
--- ProgrammingforAI/CA/analyziz.py
+++
@@ -1,9 +0,0 @@
-# Code developed by Renato Francisco Goedert
-# Sutudent Number: x25115766
-
-import pandas as pd
-import numpy as np
-from colorama import Fore, Style, init
-
-# Initialising Colorama, I like Colors, dont judge me!
-init(autoreset=True)
