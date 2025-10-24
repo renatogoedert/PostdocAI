2b4fac84644be63c2dead3d5078b3c70c8a3423e
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-10-24 14:52:30 +0100

    adding comments

--- ProgrammingforAI/CA/analysis.py
+++ ProgrammingforAI/CA/analysis.py
@@ -137,11 +137,13 @@ if df['QuizParticipation'].isnull().sum() > 0 or df['PastPerformance'].isnull().
     
     performance_mean = df['PastPerformance'].mean()
 
-    print(performance_mean, participation_mean)
-
-    df.loc[missing_participation_index, 'QuizParticipation'] = df.loc[missing_participation_index, 'PastPerformance'] * participation_mean / performance_mean
-
-    df.loc[missing_performance_index, 'PastPerformance'] = df.loc[missing_performance_index, 'QuizParticipation'] * performance_mean / participation_mean
+    #Check is there is Quiz Participation values to be added and calculate them
+    if not missing_participation_index.empty:
+        df.loc[missing_participation_index, 'QuizParticipation'] = df.loc[missing_participation_index, 'PastPerformance'] * participation_mean / performance_mean
+    
+    #Check is there is PastPerformance values to be added and calculate them
+    if not missing_performance_index.empty:
+        df.loc[missing_performance_index, 'PastPerformance'] = df.loc[missing_performance_index, 'QuizParticipation'] * performance_mean / participation_mean
 
     
 else:
