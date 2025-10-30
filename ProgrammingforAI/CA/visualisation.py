e28beda3b0b6658d5818ee94d14a445814a2c748
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-10-30 17:47:43 +0000

    adding part_4

---
+++ ProgrammingforAI/CA/visualisation.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from colorama import Fore, init
+
+# Initialising Colorama, I like Colors, dont judge me!
+init(autoreset=True)
