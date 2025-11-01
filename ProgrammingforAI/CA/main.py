f33b8fc42a1d35e9e23a6be2f217295458667c83
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-11-01 13:58:13 +0000

    fixing small bugs

--- ProgrammingforAI/CA/main.py
+++ ProgrammingforAI/CA/main.py
@@ -14,50 +14,50 @@ def main():
     Runs the complete data analysis pipeline from start to finish.
     """
     init_colors()
-    print(f"{Fore.CYAN}{'='*40}")
-    print(f"{Fore.CYAN}== NCI STUDENT ANALYSIS PIPELINE START ==")
-    print(f"{Fore.CYAN}{'='*40}")
+    print(f"{Fore.CYAN}{'='*51}")
+    print(f"{Fore.CYAN}== NCI STUDENT x25115766 ANALYSIS PIPELINE START ==")
+    print(f"{Fore.CYAN}{'='*51}")
     
     # Part 1: Data Generation
     print(f"\n{Fore.GREEN}--- Executing Part 1: Data Generation ---")
     student_data.main()
     time.sleep(1)
-    print(f"\nPart 2 Running in 3 sec, press Ctrl+D to stop")
+    print(f"\nPart 2 Running in 3 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 2 Running in 2 sec, press Ctrl+D to stop")
+    print(f"Part 2 Running in 2 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 2 Running in 1 sec, press Ctrl+D to stop")
+    print(f"Part 2 Running in 1 sec, press Ctrl+C to stop")
     time.sleep(1)
 
     # Part 2: Data Wrangling
     print(f"\n{Fore.GREEN}--- Executing Part 2: Data Wrangling ---")
     analysis.main()
     time.sleep(1)
-    print(f"\nPart 3 Running in 3 sec, press Ctrl+D to stop")
+    print(f"\nPart 3 Running in 3 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 3 Running in 2 sec, press Ctrl+D to stop")
+    print(f"Part 3 Running in 2 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 3 Running in 1 sec, press Ctrl+D to stop")
+    print(f"Part 3 Running in 1 sec, press Ctrl+C to stop")
     time.sleep(1)
     
     # Part 3: Data Analysis
     print(f"\n{Fore.GREEN}--- Executing Part 3: Data Analysis ---")
     part_3.main()
     time.sleep(1)
-    print(f"\nPart 4 Running in 3 sec, press Ctrl+D to stop")
+    print(f"\nPart 4 Running in 3 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 4 Running in 2 sec, press Ctrl+D to stop")
+    print(f"Part 4 Running in 2 sec, press Ctrl+C to stop")
     time.sleep(1)
-    print(f"Part 4 Running in 1 sec, press Ctrl+D to stop")
+    print(f"Part 4 Running in 1 sec, press Ctrl+C to stop")
     time.sleep(1)
     
     # Part 4: Data Visualisation
     print(f"\n{Fore.GREEN}--- Executing Part 4: Data Visualisation ---")
     visualisation.main()
     
-    print(f"\n{Fore.CYAN}{'='*42}")
+    print(f"\n{Fore.CYAN}{'='*44}")
     print(f"{Fore.CYAN}== PIPELINE FINISHED SUCCESSFULLY (PLOTS) ==")
-    print(f"{Fore.CYAN}{'='*42}")
+    print(f"{Fore.CYAN}{'='*44}")
 
 if __name__ == "__main__":
     main()
